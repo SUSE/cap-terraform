f92557b2a308c89b83caf45ae7a928ec52794c48
Satadru Roy <sroy@suse.com>
2019-07-17 14:56:50 -0700

    Remove setting ext-dns annotations in e2e branch

--- ext-dns-cf-svc-annotate.sh
+++
@@ -1,3 +0,0 @@
-kubectl annotate svc router-gorouter-public -n ${NS} "external-dns.alpha.kubernetes.io/hostname=${DOMAIN}, *.${DOMAIN}"
-kubectl annotate svc diego-ssh-ssh-proxy-public -n ${NS} "external-dns.alpha.kubernetes.io/hostname=ssh.${DOMAIN}"
-kubectl annotate svc tcp-router-tcp-router-public -n ${NS} "external-dns.alpha.kubernetes.io/hostname=*.tcp.${DOMAIN}, tcp.${DOMAIN}"
--- ext-dns-uaa-svc-annotate.sh
+++
@@ -1,1 +0,0 @@
-kubectl annotate svc uaa-uaa-public -n ${NS} "external-dns.alpha.kubernetes.io/hostname=uaa.${DOMAIN}, *.uaa.${DOMAIN}"
