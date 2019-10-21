d25b0fb972a693bba7d401789ce12eeaf9f42aa9
Satadru Roy <sroy@suse.com>
2019-10-21 13:53:20 +0200

    Use Route53FullAccess as a workaround for external-dns IAM policy issues

--- eks/modules/eks/worker-iam.tf
+++ eks/modules/eks/worker-iam.tf
@@ -12,13 +12,19 @@ data "aws_iam_policy_document" "worker-role-policy" {
       identifiers = ["ec2.amazonaws.com"]
     }
   }
-  // Need to add another statement here for the external-dns IAM policy for hosted zones
-
-  statement {
-    actions   = ["route53:ChangeResourceRecordSets"]
-    resources = ["arn:aws:route53:::hostedzone/${data.aws_route53_zone.selected.zone_id}"]
-  }
 }
+
+data "aws_iam_policy_document" "route53-restricted-update-policy" {
+    statement {
+        actions   = ["route53:ChangeResourceRecordSets"]
+        resources = ["arn:aws:route53:::hostedzone/${data.aws_route53_zone.selected.zone_id}"]
+    }   
+}
+
+/*resource "aws_iam_policy" "route-53-update-policy" {
+    name = "restricted-route53-policy"
+    policy = "${data.aws_iam_policy_document.route53-restricted-update-policy.json}"
+}*/
 
 resource "aws_iam_role" "aws-node" {
   name = "${var.cluster_name}-worker-iam-role"
@@ -44,6 +50,17 @@ resource "aws_iam_role_policy_attachment" "aws-node-AmazonRoute53ReadOnlyAccess"
  policy_arn = "arn:aws:iam::aws:policy/AmazonRoute53ReadOnlyAccess"
  role       = "${aws_iam_role.aws-node.name}"
 }
+
+resource "aws_iam_role_policy_attachment" "aws-node-AmazonRoute53FullAccess" {
+ policy_arn = "arn:aws:iam::aws:policy/AmazonRoute53FullAccess"
+ role       = "${aws_iam_role.aws-node.name}"
+}
+
+/* resource "aws_iam_role_policy_attachment" "aws-node-selectiveRoute53UpdateAccess" {
+    policy_arn = "${aws_iam_policy.route-53-update-policy.arn}"
+    role       = "${aws_iam_role.aws-node.name}"
+
+} */
 
 resource "aws_iam_instance_profile" "aws-node" {
   name = "${var.cluster_name}-worker-iam-instance-profile"
--- eks/modules/services/k8s.tf
+++ eks/modules/services/k8s.tf
@@ -13,3 +13,6 @@ provider "kubernetes" {
   cluster_ca_certificate = "${base64decode(data.aws_eks_cluster.eks.certificate_authority.0.data)}"
   token = "${data.aws_eks_cluster_auth.eks-auth.token}"
 }
+
+//To do
+// Add a storage class resource with name "persistent"
