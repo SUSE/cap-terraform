829704ed81a7c3bff3b92b82305944410f038da2
Rania Mohamed <Rania.Mohamed@suse.com>
2019-11-22 17:42:04 +0100

    Fixes for the subscription

--- aks/setup_cert_issuer.sh
+++ aks/setup_cert_issuer.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 az login --service-principal -u $ARM_CLIENT_ID -p $ARM_CLIENT_SECRET --tenant $ARM_TENANT_ID
 
-az aks get-credentials -n ${CLUSTER_NAME} -g ${RESOURCE_GROUP} -a -f /tmp/aksk8scfg --subscription ARM_SUBSCRIPTION_ID
+az aks get-credentials -n ${CLUSTER_NAME} -g ${RESOURCE_GROUP} -a -f /tmp/aksk8scfg --subscription $ARM_SUBSCRIPTION_ID
 export KUBECONFIG=/tmp/aksk8scfg
 
 kubectl create secret generic -n cert-manager azuredns-config --from-literal=CLIENT_SECRET=${AZ_CERT_MGR_SP_PWD}
