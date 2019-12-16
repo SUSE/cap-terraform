ad94de5c10670501e5cdd6a29fafd7b2066ade1f
Rasadus03 <rasadus@gmail.com>
2019-12-16 11:01:13 +0100

    Update deploy_metrics.sh

--- aks/deploy_metrics.sh
+++ aks/deploy_metrics.sh
@@ -5,6 +5,6 @@ export KUBECONFIG=./aksk8scfg
 APISRV="$(kubectl cluster-info| head -n 1|grep -o ' at .*$'| cut -f3 -d' '| cut -f3 -d'/'|cut -f1 -d':')"
 cat $METRICS_FILE $SCF_FILE > ./capConfigurationFile.yaml
 sed -i "s/MASTER_URL/https:\/\/$APISRV/g"  ./capConfigurationFile.yaml
-helm install ./metrics     --name susecf-metrics     --namespace metrics    --values ./capConfigurationFile.yaml 
+helm install suse/metrics     --name susecf-metrics     --namespace metrics    --values ./capConfigurationFile.yaml 
 #rm -f /tmp/aksk8scfg2
 #az logout
