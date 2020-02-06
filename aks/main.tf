b910e1e4ffff636b0e5bf47b9dccbacbc4fdda4a
James Mason <jmason@suse.com>
2020-02-06 13:26:02 -0800

    Inline the SSH key from a variable instead of an external file

--- aks/main.tf
+++ aks/main.tf
@@ -16,7 +16,7 @@ resource "azurerm_kubernetes_cluster" "k8s" {
         admin_username = "${var.agent_admin}"
 
         ssh_key {
-            key_data = "${file("${var.ssh_public_key}")}"
+            key_data = "${var.ssh_public_key}"
         }
     }
 
