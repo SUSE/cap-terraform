594b3fbcb271890df9c460faed810dac75a61b38
James Mason <jmason@suse.com>
2020-03-20 17:09:55 -0700

    Remove sources that are not public-cloud ready (#54)

--- eks/modules/services/setup_cert_issuer.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/sh
-export KUBECONFIG=$KUBECONFIG
-
-kubectl apply -f modules/services/le-prod-cert-issuer.yaml
--- gke/deploy_metrics.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/sh
-export APISRV="$(kubectl cluster-info| head -n 1|grep -o ' at .*$'| cut -f3 -d' '| cut -f3 -d'/'|cut -f1 -d':' | sed 's/\x1b\[[0-9;]*m//g')"
-cat $METRICS_FILE $SCF_FILE > ./capConfigurationFile.yaml
-sed -i "s/MASTER_URL/https:\/\/$APISRV/g"  ./capConfigurationFile.yaml
-helm install suse/metrics     --name susecf-metrics     --namespace metrics    --values ./capConfigurationFile.yaml 
