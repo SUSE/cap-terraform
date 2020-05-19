265634dafe1aefb4580884cf621a6f0e096ac859
James Mason <jmason@suse.com>
2020-05-18 18:07:43 -0700

    Provide `helm` with kubeconfig path

--- aks/helm.tf
+++ aks/helm.tf
@@ -6,7 +6,7 @@ provider "helm" {
     client_certificate = base64decode(azurerm_kubernetes_cluster.k8s.kube_config[0].client_certificate)
     client_key = base64decode(azurerm_kubernetes_cluster.k8s.kube_config[0].client_key)
     cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.k8s.kube_config[0].cluster_ca_certificate)
+    config_path = local.kubeconfig_file_path
   }
-  
 }
 
