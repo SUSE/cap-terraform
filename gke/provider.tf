cc499453d8cbcff815bb2b65fff4e6f72e67650d
James Mason <jmason@suse.com>
2020-07-09 15:45:06 -0700

    Pin terraform provider versions

--- gke/provider.tf
+++ gke/provider.tf
@@ -6,3 +6,14 @@ provider "google" {
   project     = var.project
 }
 
+provider "local" {
+  version = "1.4.0"
+}
+
+provider "null" {
+  version = "2.1.2"
+}
+
+provider "random" {
+  version = "2.2.1"
+}
