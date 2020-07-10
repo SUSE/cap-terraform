947e7b32b7d34ad73cb4acae478e95a5beab3f36
James Mason <jmason@suse.com>
2020-07-10 10:34:10 -0700

    Require at least k8s v 1.15

--- gke/main.tf
+++ gke/main.tf
@@ -12,6 +12,7 @@ locals {
 resource "google_container_cluster" "gke-cluster" {
   name     = "cap-${random_string.cluster_name.result}"
   location = var.location
+  min_master_version = "1.15"
 
   # We can't create a cluster with no node pool defined, but we want to only use
   # separately managed node pools. So we create the smallest possible default
