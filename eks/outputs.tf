b707e479efb689bbe24063a2789d356aafbf0ff4
James Mason <jmason@suse.com>
2020-07-28 09:48:37 -0700

    Add common URLs as outputs

--- eks/outputs.tf
+++ eks/outputs.tf
@@ -12,3 +12,13 @@ output "path-to-kubeconfig" {
 output "aws-route53-hostedzone-policy" {
   value = module.eks.aws_route53_hosted_zone_policy
 }
+
+output "uaa_url" {
+  value = "https://uaa.${var.cap_domain}/login"
+}
+output "stratos_url" {
+  value = "https://stratos.${var.cap_domain}"
+}
+output "metrics_url" {
+  value = "https://metrics.${var.cap_domain}"
+}
