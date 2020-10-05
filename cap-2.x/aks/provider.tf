0bf58709bc85570010bf45cd3c74e31803e9014f
James Mason <jmason@suse.com>
2020-10-05 11:53:40 -0700

    autoformat

--- cap-2.x/aks/provider.tf
+++ cap-2.x/aks/provider.tf
@@ -5,10 +5,10 @@ terraform {
 provider "azurerm" {
   version = "2.4.0"
   features {}
-  subscription_id = var.subscription_id
-  client_id       = var.client_id
-  client_secret   = var.client_secret
-  tenant_id       = var.tenant_id
+  subscription_id            = var.subscription_id
+  client_id                  = var.client_id
+  client_secret              = var.client_secret
+  tenant_id                  = var.tenant_id
   skip_provider_registration = true
 }
 
