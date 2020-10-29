594602f89730cce8aa275891b0d081da62fdd8d9
Victor Cuadrado Juan <viccuad@users.noreply.github.com>
2020-10-29 22:57:58 +0100

    Add Calico as networking plugin to GKE (#98)
    * Enable networking plugin for GKE
    
    * Fix style on gke/main.tf
    
    * Enable network_policy with Calico on GKE
    
    Signed-off-by: Víctor Cuadrado Juan <vcuadradojuan@suse.de>
    
    Co-authored-by: Christian Richter <crichter@suse.com>

--- gke/main.tf
+++ gke/main.tf
@@ -8,7 +8,7 @@ resource "google_container_cluster" "gke-cluster" {
   remove_default_node_pool = true
 
   initial_node_count = 1
-  resource_labels = var.cluster_labels
+  resource_labels    = var.cluster_labels
 
   # Setting an empty username and password explicitly disables basic auth
   master_auth {
@@ -24,7 +24,14 @@ resource "google_container_cluster" "gke-cluster" {
     horizontal_pod_autoscaling {
       disabled = true
     }
+    network_policy_config {
+      disabled = false
+    }
+  }
 
+  network_policy {
+    enabled  = true
+    provider = "CALICO"
   }
 }
 
