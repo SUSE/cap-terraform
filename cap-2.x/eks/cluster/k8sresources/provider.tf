84cb8597e0538029e2881d82ee1f33d6f47a7bd1
James Mason <jmason@suse.com>
2020-12-09 12:02:33 -0800

    Fix kubernetes provider to packaged version.

--- cap-2.x/eks/cluster/k8sresources/provider.tf
+++ cap-2.x/eks/cluster/k8sresources/provider.tf
@@ -1,5 +1,5 @@
 provider "kubernetes" {
-  version                = "1.13.3"
+  version                = "1.13.2"
   load_config_file       = false
   host                   = var.aws_eks_cluster_endpoint
   cluster_ca_certificate = base64decode(var.aws_eks_cluster_certificate_authority_data)
