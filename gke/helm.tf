ca302773f02a5214196fe567f8d5fb1655f8c0d4
Víctor Cuadrado Juan <vcuadradojuan@suse.de>
2021-01-28 10:43:29 +0100

    Drop load_config_file config in helm.tf
    Not needed anymore, default to reading KUBECONFIG
    https://registry.terraform.io/providers/hashicorp/helm/latest/docs#file-config

--- gke/helm.tf
+++ gke/helm.tf
@@ -2,7 +2,6 @@ provider "helm" {
     version = "~> 2.0.2"
 
   kubernetes {   
-    load_config_file = false
     host = "https://${google_container_cluster.gke-cluster.endpoint}"
     cluster_ca_certificate = base64decode(google_container_cluster.gke-cluster.master_auth.0.cluster_ca_certificate)
     token = data.google_client_config.current.access_token
